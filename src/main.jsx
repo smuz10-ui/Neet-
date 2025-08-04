2692c40581f8e352fcfe2352676eab44a23532e1
smuz10-ui <smuzbotgalaxy@gmail.com>
2025-08-04 17:53:22 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PyqWebsite from "./PyqWebsite";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PyqWebsite />
+  </React.StrictMode>
+);
